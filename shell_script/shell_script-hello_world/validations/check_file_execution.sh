803f6cb4a871dac5dafc120cc54bf3cd18387418
Ramon Borges <ramon.borges@telekom.com>
2021-06-23 14:43:38 +0200

    Fixed the if clause of the test of the last case

--- shell_script/shell_script-hello_world/validations/check_file_execution.sh
+++ shell_script/shell_script-hello_world/validations/check_file_execution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXECUTED=$(history | awk '{$1="";print substr($0,2)}' | grep -E '^bash /root/shell/my_first_shell.sh$')
-if [ ! -z "$EXECUTED" ]; then
+if [ -z "$EXECUTED" ]; then
 	echo "done"
 else
 	exit 2
