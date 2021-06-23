77a5216c3dd571ec49b5c0ff2865107605e480c8
Ramon Borges <ramon.borges@telekom.com>
2021-06-23 08:54:38 +0200

    Fixed the return and the chmod of the test script

--- shell_script/shell_script-hello_world/assets/validations/check_file_existence.sh
+++ shell_script/shell_script-hello_world/assets/validations/check_file_existence.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -s /root/shell/my_first_shell.sh ] then
-	return 1
+if [ ! -s /root/shell/my_first_shell.sh ]; then
+  exit 1
 fi
 
 LINES=$(wc -l /root/shell/my_first_shell.sh | awk -F' ' '{print $1}')
-if [ LINES  -ne 3 ] then
-	return 2
+if [ "$LINES" -ne "3" ]; then
+  exit 2
 fi
