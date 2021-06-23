ed139a7a0deff8a79ab9946e8cc8cff71fb389ad
Ramon Borges <ramon.borges@telekom.com>
2021-06-23 13:03:40 +0200

    Changes in the test return value

--- shell_script/shell_script-hello_world/validations/check_file_existence.sh
+++ shell_script/shell_script-hello_world/validations/check_file_existence.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ ! -s /root/shell/my_first_shell.sh ]; then
-  echo "undone"
+  exit 1
 fi
 
 LINES=$(wc -l /root/shell/my_first_shell.sh | awk -F' ' '{print $1}')
 if [ "$LINES" -ne "3" ]; then
-  echo "undone"
+  exit 2
 fi
 
 echo "done"
--- shell_script/shell_script-hello_world/validations/check_file_permission.sh
+++ shell_script/shell_script-hello_world/validations/check_file_permission.sh
@@ -5,5 +5,5 @@ FILE="/root/shell/my_first_shell.sh"
 if [ -x "$FILE" && -r "$FILE" ]; then
 	echo "done"
 else
-	echo "undone"
+	exit 1
 fi
