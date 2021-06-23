85f585339f43b50a999cd0b74815e1ca60f4e9d9
Ramon Borges <ramon.borges@telekom.com>
2021-06-23 15:06:20 +0200

    Fixed the last step content

--- shell_script/shell_script-hello_world/validations/check_file_execution.sh
+++ shell_script/shell_script-hello_world/validations/check_file_execution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXECUTED=$(history | awk '{$1="";print substr($0,2)}' | grep -E '^bash /root/shell/my_first_shell.sh$')
-if [ -z "$EXECUTED" ]; then
+if [[ -z "$EXECUTED" ]]; then
 	exit 2
 else
 	echo "done"
