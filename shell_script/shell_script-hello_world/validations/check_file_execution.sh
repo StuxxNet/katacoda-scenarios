1f33acd10c9baaab049a2631e66d9d16a940e62d
Ramon Borges <ramon.borges@telekom.com>
2021-06-23 14:39:49 +0200

    Fixed the test result of the last step

--- shell_script/shell_script-hello_world/validations/check_file_execution.sh
+++ shell_script/shell_script-hello_world/validations/check_file_execution.sh
@@ -2,7 +2,7 @@
 
 EXECUTED=$(history | awk '{$1="";print substr($0,2)}' | grep -E '^bash /root/shell/my_first_shell.sh$')
 if [ ! -z "$EXECUTED" ]; then
-	echo "Done"
+	echo "done"
 else
 	exit 2
 fi
