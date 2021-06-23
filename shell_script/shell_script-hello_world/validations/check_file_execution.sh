e8b931af49bedef9e4a43387e98e26076abe10d6
Ramon Borges <ramon.borges@telekom.com>
2021-06-23 14:47:58 +0200

    Changed the index and the testing of the last step of the first class

--- shell_script/shell_script-hello_world/validations/check_file_execution.sh
+++ shell_script/shell_script-hello_world/validations/check_file_execution.sh
@@ -2,7 +2,7 @@
 
 EXECUTED=$(history | awk '{$1="";print substr($0,2)}' | grep -E '^bash /root/shell/my_first_shell.sh$')
 if [ -z "$EXECUTED" ]; then
-	echo "done"
-else
 	exit 2
+else
+	echo "done"
 fi
